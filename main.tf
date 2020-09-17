733b2e42d0ce6e6d6851503f1ab5e69e576a35b7
Tomas Basham <tomasbasham@gmail.com>
2020-09-17 11:42:06 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -123,8 +123,8 @@ resource aws_instance "hashicat" {
   vpc_security_group_ids      = [aws_security_group.hashicat.id]
 
   tags = {
-    name = "${var.prefix}-hashicat-instance"
-    billable = "true"
+    name = "${var.prefix}-hashicat-instance",
+    billable = "true",
     department = "devops"
   }
 }
